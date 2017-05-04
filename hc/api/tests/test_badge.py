670bf7f032ec634f697fa550243e813c9850627d
Sharonsyra <sharonwaithii@gmail.com>
2017-05-04 14:39:14 +0300

    [Chore] used .format instead of string formatting

--- hc/api/tests/test_badge.py
+++ hc/api/tests/test_badge.py
@@ -12,7 +12,7 @@ class BadgeTestCase(BaseTestCase):
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
 
     def test_it_rejects_bad_signature(self):
-        resp = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
+        resp = self.client.get("/badge/{}/12345678/foo.svg" .format(self.alice.username))
 
         self.assertEqual(resp.status_code, 404)
         # Assert the expected response status code
@@ -20,8 +20,7 @@ class BadgeTestCase(BaseTestCase):
     def test_it_returns_svg(self):
         sig = base64_hmac(str(self.alice.username), "foo", settings.SECRET_KEY)
         sig = sig[:8].decode("utf-8")
-        url = "/badge/%s/%s/foo.svg" % (self.alice.username, sig)
-
+        url = "/badge/{}/{}/foo.svg".format(self.alice.username, svg)
         resp = self.client.get(url)
 
         self.assertTrue("svg", resp.templates)
