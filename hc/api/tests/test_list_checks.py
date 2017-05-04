f261097486b5d425f1d41ba329e207aafbb62f2a
Caroline Wanjiku <Caroline Wanjiku>
2017-05-04 15:30:15 +0300

    [Chore]Replace hard code

--- hc/api/tests/test_list_checks.py
+++ hc/api/tests/test_list_checks.py
@@ -37,35 +37,38 @@ class ListChecksTestCase(BaseTestCase):
         # ## Assert the response status code
         self.assertEqual(resp.status_code, 200)
         doc = resp.json()
-        #self.assertTrue("checks" in doc)
+        # self.assertTrue("checks" in doc)
         checks = {check["name"]: check for check in doc["checks"]}
         # ## Assert the expected length of checks
 
         self.assertEqual(len(doc["checks"]), 2)
-        # ## Assert the checks Alice 1 and Alice 2's timeout, grace, ping_url, status,
+        # ## Assert the checks Alice 1 and Alice 2's timeout, grace, ping_url,
+        #  status,
         # Alice 1
         self.assertEqual(checks['Alice 1']['grace'], 900)
         self.assertEqual(checks['Alice 1']['timeout'], 3600)
-        self.assertNotEqual(checks['Alice 1']['ping_url'],
-                            'https://hc-bs-staging.herokuapp.com//ping/0af1d589-e5e8-4e26-83cf-a38929136f3f')
+        self.assertEqual(checks['Alice 1']['ping_url'],
+                         self.a1.url())
         self.assertEqual(checks['Alice 1']['status'], 'new')
         # Alice 2
         self.assertEqual(checks['Alice 2']['grace'], 3600)
         self.assertEqual(checks['Alice 2']['timeout'], 86400)
-        self.assertNotEqual(checks['Alice 2']['ping_url'],
-                            'https://hc-bs-staging.herokuapp.com//ping/2bf1aa96-e8d8-4d0a-9408-d33d0829907e')
+        self.assertEqual(checks['Alice 2']['ping_url'],
+                         self.a2.url())
         self.assertEqual(checks['Alice 2']['status'], 'up')
         # ## last_ping, n_pings and pause_url
         # Alice 1
-        self.assertEqual(checks['Alice 1']['last_ping'], self.a2.last_ping.isoformat())
+        self.assertEqual(checks['Alice 1']['last_ping'],
+                         self.a1.last_ping.isoformat())
         self.assertEqual(checks['Alice 1']['n_pings'], self.a1.n_pings)
         self.assertNotEqual(checks['Alice 1']['pause_url'],
-                            'https://hc-bs-staging.herokuapp.com//api/v1/checks/0af1d589-e5e8-4e26-83cf-a38929136f3f/pause')
+                            self.a1.url())
         # Alice 2
-        self.assertEqual(checks['Alice 2']['last_ping'], self.a2.last_ping.isoformat())
+        self.assertEqual(checks['Alice 2']['last_ping'],
+                         self.a2.last_ping.isoformat())
         self.assertEqual(checks['Alice 2']['n_pings'], 0)
         self.assertNotEqual(checks['Alice 2']['pause_url'],
-                            'https://hc-bs-staging.herokuapp.com//api/v1/checks/2bf1aa96-e8d8-4d0a-9408-d33d0829907e/pause')
+                            self.a2.url())
 
     def test_it_shows_only_users_checks(self):
         bobs_check = Check(user=self.bob, name="Bob 1")
