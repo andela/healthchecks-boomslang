c302d9c47d67b83e465c4ae3e02b7a5384fe64ea
Joan Awinja Ingari <joan.awinja@andela.com>
2017-05-09 13:05:52 +0300

    #143401483 Add Account Tests  (#12)
    * [account-test] test_login,test_switch_team,test_profile
    
    * [account-test] test_check_token,test_profile
    
    * [account-test] test_check_token,test_profile(corrections)
    
    * [account-test] test_team_access_ware
    
    * [account-test] test_login
    
    * [account-test] test_team_acces_middleware,test_login(corrections)
    
    * PR review rectifications
    
    * [Feature #143401483]Correct assertions
    
    * [Feature #143401483]Correct assertions]
    
    * [Feature #143401483]Correct assertions]

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -11,31 +11,34 @@ class SwitchTeamTestCase(BaseTestCase):
         self.client.login(username="bob@example.org", password="password")
 
         url = "/accounts/switch_team/%s/" % self.alice.username
-        r = self.client.get(url, follow=True)
+        response = self.client.get(url, follow=True)
 
         ### Assert the contents of r
+        self.assertEqual(response.status_code, 200)
 
     def test_it_checks_team_membership(self):
         self.client.login(username="charlie@example.org", password="password")
 
         url = "/accounts/switch_team/%s/" % self.alice.username
-        r = self.client.get(url)
+        response = self.client.get(url)
         ### Assert the expected error code
+        self.assertEqual(response.status_code, 403)
 
     def test_it_switches_to_own_team(self):
         self.client.login(username="alice@example.org", password="password")
 
         url = "/accounts/switch_team/%s/" % self.alice.username
-        r = self.client.get(url, follow=True)
+        response = self.client.get(url, follow=True)
 
         ### Assert the expected response code
+        self.assertEqual(response.status_code, 200)
 
     def test_it_handles_invalid_username(self):
         self.client.login(username="bob@example.org", password="password")
 
         url = "/accounts/switch_team/dave/"
-        r = self.client.get(url)
-        self.assertEqual(r.status_code, 403)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 403)
 
     def test_it_requires_login(self):
         url = "/accounts/switch_team/%s/" % self.alice.username
