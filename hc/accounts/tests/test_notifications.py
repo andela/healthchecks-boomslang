aab9e40a4a61cc6aa82f4942e705b946bb257f3c
r-wambui <rose.wambui@andela.com>
2017-05-17 17:02:52 +0300

    remove assert false as it is invalid

--- hc/accounts/tests/test_notifications.py
+++ hc/accounts/tests/test_notifications.py
@@ -13,12 +13,3 @@ class NotificationsTestCase(BaseTestCase):
         self.alice.profile.refresh_from_db()
         self.assertTrue(self.alice.profile.reports_allowed)
 
-    def test_it_saves_reports_allowed_false(self):
-        self.client.login(username="alice@example.org", password="password")
-
-        form = {}
-        r = self.client.post("/accounts/profile/notifications/", form)
-        assert r.status_code == 200
-
-        self.alice.profile.refresh_from_db()
-        self.assertFalse(self.alice.profile.reports_allowed)
